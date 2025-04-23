f2d0a7a8fc2ff1f7a7f976112c590dea99753c42
Frédéric TAIEB <frederictaieb@Frederics-MacBook-Pro.local>
2025-04-23 20:12:57 +0200

    navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-lg">
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
