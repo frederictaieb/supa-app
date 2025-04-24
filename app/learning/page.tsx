bab603384a102bb92839156c0a74f6e9379416b5
Frédéric TAIEB <frederictaieb@Frederics-MacBook-Pro.local>
2025-04-24 10:38:45 +0200

    learning react

--- app/learning/page.tsx
+++ app/learning/page.tsx
@@ -1,16 +1,23 @@
 "use client"
 import { useState } from "react"
+import Image from 'next/image'
 
 const Test = () => { 
 
     const [number, setNumber] = useState(0)
 
     return(
-        <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-            <img class="size-12 shrink-0" src="/img/logo.svg" alt="ChitChat Logo" />
+        <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">          
+            <Image 
+                className="size-12 shrink-0" 
+                src="/img/logo.svg" 
+                alt="ChitChat Logo"
+                width={48}
+                height={48}
+            />
             <div>
-                <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
-                <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+                <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
+                <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
                 <div>Hello, le compteur est {number}</div>
                 <div>
                     <button onClick={() => setNumber(number+1)}>Increment</button>
