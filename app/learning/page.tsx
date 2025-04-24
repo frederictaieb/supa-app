63d97bc9bbab33d86d7097bfb585fbdfed35c151
Frédéric TAIEB <frederictaieb@Frederics-MacBook-Pro.local>
2025-04-24 10:45:01 +0200

    learning react

--- app/learning/page.tsx
+++ app/learning/page.tsx
@@ -10,7 +10,7 @@ const Test = () => {
         <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">          
             <Image 
                 className="size-12 shrink-0" 
-                src="/img/logo.svg" 
+                src="/img/logo.jpg" 
                 alt="ChitChat Logo"
                 width={48}
                 height={48}
