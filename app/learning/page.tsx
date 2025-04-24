8b8911044bc0d08b4f2e4365eea6615c56fb2969
Frédéric TAIEB <frederictaieb@Frederics-MacBook-Pro.local>
2025-04-24 11:59:38 +0200

    Learning

--- app/learning/page.tsx
+++ app/learning/page.tsx
@@ -1,20 +1,16 @@
 "use client"
 //import { useState } from "react"
 //import Image from 'next/image'
-import Link from 'next/link'
+//import Link from 'next/link'
+import Form from 'next/form'
 
 export default function Test() {
-  return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-500 flex items-center justify-center p-4">
-      <div className="bg-white rounded-lg shadow-xl p-6 max-w-sm w-full hover:scale-105 transition-transform">
-        <h1 className="text-2xl font-bold text-gray-800 mb-4">Test Tailwind</h1>
-        <p className="text-gray-600">
-          Si vous voyez cette carte avec un dégradé en arrière-plan, des ombres, et une animation au survol, Tailwind fonctionne correctement ! 🎉
-        </p>
-      </div>
-      <div>
-        <Link href="../profile">Profile</Link>
-      </div>
-    </div>
-  )
+    return (
+        <Form action="/search">
+          {/* On submission, the input value will be appended to
+              the URL, e.g. /search?query=abc */}
+          <input name="query" />
+          <button type="submit">Submit</button>
+        </Form>
+      )
 }
