95ddee9e7153275cdc732e29b3bba3b161e1673d
Frédéric TAIEB <frederictaieb@Frederics-MacBook-Pro.local>
2025-04-24 10:48:39 +0200

    learning react

--- app/learning/page.tsx
+++ app/learning/page.tsx
@@ -7,6 +7,7 @@ const Test = () => {
     const [number, setNumber] = useState(0)
 
     return(
+        <div>
         <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">          
             <Image 
                 className="size-12 shrink-0" 
@@ -23,6 +24,19 @@ const Test = () => {
                     <button onClick={() => setNumber(number+1)}>Increment</button>
                 </div>
             </div>
+        </div>
+        <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+            <img className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/logo.jpg" alt="" />
+            <div className="space-y-2 text-center sm:text-left">
+                <div className="space-y-0.5">
+                    <p className="text-lg font-semibold text-black">Erin Lindford</p>
+                    <p className="font-medium text-gray-500">Product Engineer</p>
+                </div>
+                <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+                    Message
+                </button>
+            </div>
+        </div>
         </div>
     )
 }
